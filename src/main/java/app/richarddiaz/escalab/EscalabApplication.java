1f603f98f189844dc5fd9642ab0822e67db91835
rokiman33 <richardddiaz.esfera@gmail.com>
2023-06-03 01:24:39 -0400

    Initial commit

---
+++ src/main/java/app/richarddiaz/escalab/EscalabApplication.java
@@ -0,0 +1,13 @@
+package app.richarddiaz.escalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscalabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscalabApplication.class, args);
+	}
+
+}
